8613488ce0147c6a393409b97ecdedb3bea1c724
seanzira <162341287+seanzira@users.noreply.github.com>
2024-11-16 08:52:46 +0200

    Update Ou.js

--- models/Ou.js
+++ models/Ou.js
@@ -4,7 +4,6 @@ import mongoose from 'mongoose';
 const OUSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String },
-  divisions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Division' }]
 }, { timestamps: true });
 
 // Export OU model to be used in other parts of the application
